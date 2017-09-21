1ad2e6163eb3e3070da13ca6f8422849b1961da3
Hollydan <2642956839@qq.com>
2017-09-21 04:34:42 +0000

    orderpicture

--- src/Handlers/AllPictureHandler.php
+++ src/Handlers/AllPictureHandler.php
@@ -31,8 +31,33 @@ class AllPictureHandler extends Handler
             'picture_perpage.required' => '请输入每页显示条数',
         ]);
 
+        //图片排序
+        $order = $this->request->get('picture_order', 1);
+        switch ($order) {
+            case 1:     //按上传时间从晚到早
+                $order = 'created_at';
+                $destination = 'desc';
+                break;
+            case 2:     //按上传时间从早到晚
+                $order = 'created_at';
+                $destination = 'asc';
+                break;
+            case 3:     //按图片从大到小
+                $order = 'size';
+                $destination = 'desc';
+                break;
+            case 4:     //按图片从小到大
+                $order = 'size';
+                $destination = 'asc';
+                break;
+            case 5:     //按图片名
+                $order = 'name';
+                $destination = 'asc';
+                break;
+        }
+
         $perPage = $this->request->get('picture_perpage');
-        $pictures = Picture::OrderBy('created_at', 'desc')->paginate($perPage);
+        $pictures = Picture::OrderBy($order, $destination)->paginate($perPage);
 
         return $this->withCode(200)->withData($pictures)->withMessage('获取数据成功！');
     }

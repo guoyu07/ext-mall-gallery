0e3ed3b6e96ad90f0d0194aa2e737045026c883b
tianjign <tfanxi@163.com>
2017-09-26 15:38:40 +0800

    add router configuration

--- resources/mixes/administration/src/mixes/router.js
+++ resources/mixes/administration/src/mixes/router.js
@@ -1,11 +1,23 @@
-import Gallery from '../pages/Gallery';
+import ProductPicture from '../pages/ProductPicture';
+import ProductPictureLook from '../pages/ProductPictureLook';
+import ProductPictureLookAll from '../pages/ProductPictureLookAll';
 
 export default function (injection) {
     injection.useExtensionRoute([
         {
             beforeEnter: injection.middleware.requireAuth,
-            component: Gallery,
+            component: ProductPicture,
             path: 'mall-gallery',
+        },
+        {
+            beforeEnter: injection.middleware.requireAuth,
+            component: ProductPictureLook,
+            path: 'product/picture/look',
+        },
+        {
+            beforeEnter: injection.middleware.requireAuth,
+            component: ProductPictureLookAll,
+            path: 'product/picture/look/all',
         },
     ]);
 }
